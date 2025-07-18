3d1aa0c34a07018067489f0a7cf9dd64e0fe58a6
2001leenasri <111949940+2001leenasri@users.noreply.github.com>
2025-07-18 00:26:03 -0700

    Update TestSauceDemo.java

--- TestSaucedemo.java
+++ TestSaucedemo.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class TestSaucedemo {
+public class TestSauceDemo {
 		
 	// main method
 		public static void main(String[] args) {
@@ -19,12 +19,15 @@ public class TestSaucedemo {
 			driver.get("https://www.saucedemo.com/v1/");
  			
 			//Id
+			// to enter user name
 			driver.findElement(By.id("user-name")).sendKeys("standard_user");
 			
 			//Id
+			// to enter password
 			driver.findElement(By.id("password")).sendKeys("secret_sauce");
 			
 			//Linktext
+			// to click login button
 			driver.findElement(By.id("login-button")).click();
 		
 				
